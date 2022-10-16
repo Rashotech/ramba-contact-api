3b0ca8d352077a2be8cce9511bf99c4b32de8cc5
Rasheed Ayoade <rashotech@gmail.com>
2022-10-16 15:27:43 +0100

    refactor - error handler

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,30 +3,19 @@ import httpStatus from "http-status";
 import ApiError from "../helpers/ApiError";
 
 const errorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    error: ApiError,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  let message;
-  let statusCode;
-  if (!(error instanceof ApiError)) {
-    statusCode = error.statusCode
-      ? httpStatus.BAD_REQUEST
-      : httpStatus.INTERNAL_SERVER_ERROR;
-    message = error.message || httpStatus[statusCode];
-  } else {
-    statusCode =
-      error.statusCode || error.status || httpStatus.INTERNAL_SERVER_ERROR;
-    message = error.message || httpStatus[statusCode];
-  }
+    const statusCode = error.statusCode || error.status || httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || httpStatus[statusCode];
+    const response = {
+        success: false,
+        message,
+    };
 
-  const response = {
-    success: false,
-    message,
-  };
-
-  return res.status(statusCode).send(response);
+    return res.status(statusCode).send(response);
 };
 
 export default errorHandler;
