a511c731adfcf36af6c742df41a0af1a3aac5d21
Rasheed Ayoade <rashotech@gmail.com>
2022-10-17 04:21:17 +0100

    feat: add contact api endpoint route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import AuthRoute from './auth.route';
+import ContactRoute from './contact.route';
 
 // API Routes
 const routes = (server: express.Application): void => {
     server.use('/api/user', new AuthRoute().router);
+    server.use('/api/contact', new ContactRoute().router);
 };
 
 export default routes;
